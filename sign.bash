KitwareMedical/slicer-macos-codesign-scripts

commit 71bab278148a946e8f7a9c3e223f19ae23a39563

Re-implement SLA attachment to avoid deprecated tools
Approach for "Extract SLA" and "Re-insert SLA" adapted
from Kitware/CMake@bed4b1583 (Utilities/Release: Add script to
sign/notarize macOS application bundle)

From Kitware/CMake@1ace60732 ((CPack/DragNDrop: Re-implement SLA attachment
to avoid deprecated tools)):

> The `Rez` tool has been deprecated since Xcode 6.  The `hdiutil flatten`
> and `hdiutil unflatten` tools have been deprecated since macOS 10.15 and
> are removed in macOS 11.  Instead use `hdiutil udifrez` to attach the
> SLA resources to disk images.  This tool accepts XML input files, so
> convert our resource file generation to produce that format.

Co-authored-by: Brad King <brad.king@kitware.com>

sign.bash
@@ -60,10 +60,16 @@ log " ${ver_major}.${ver_minor}"
 log "Backing up the original DMG"
 cp "${pkg}" "${pkg_base}.orig.dmg"
 
+log "Create temporary directory"
+temp_dir=$(mktemp -d)
+readonly temp_dir
+
 log "Extract SLA"
-hdiutil unflatten "${pkg}"
-DeRez -only 'LPic' -only 'STR#' -only 'TEXT' "${pkg}" > sla.r
-hdiutil flatten "${pkg}"
+readonly sla_xml="$temp_dir/sla.xml"
+hdiutil udifderez -xml "$pkg" > "$sla_xml"
+plutil -remove 'blkx' "$sla_xml"
+plutil -remove 'plst' "$sla_xml"
+log "Extract SLA [$sla_xml]"
 
 log "Convert from original image to uncompressed read-write"
 hdiutil convert "${pkg}" -format UDRW -o "${pkg_base}.rw"
@@ -86,10 +92,6 @@ readonly lib_dir
 lib_subdir=$(basename "${lib_dir}")
 readonly lib_subdir
 log "Library subdirectory: ${lib_subdir}"
-
-log "Create temporary directory"
-temp_dir=$(mktemp -d)
-readonly temp_dir
 
 tmp_vol_name=/Volumes/$(basename "${temp_dir}")
 readonly tmp_vol_name
@@ -236,11 +238,9 @@ log "Convert to intermediate format needed for rez tool."
 hdiutil convert "${pkg_base}.rw.dmg" -format UDRO -o "${pkg_base}.ro"
 rm -f "${pkg_base}.rw.dmg"
 
-log "Re-insert SLA with rez tool."
-hdiutil unflatten "${pkg_base}.ro.dmg"
-Rez sla.r -a -o "${pkg_base}.ro.dmg"
-hdiutil flatten "${pkg_base}.ro.dmg"
-rm -f sla.r
+log "Re-insert SLA."
+hdiutil udifrez -xml "${sla_xml}" '' "$pkg"
+rm -f "${sla_xml}"
 
 log "Convert back to read-only, compressed image."
 hdiutil convert "${pkg_base}.ro.dmg" -format UDZO -imagekey zlib-level=9 -ov -o "${pkg}"

commit e4382ade6b216c15fa1c0f1e80dc1bda1bf2e001

Fix regression in "Re-insert SLA." ensuring input exists
This commit fixes regression introduced in 71bab27 (Re-implement SLA
attachment to avoid deprecated tools) ensuring that the input of
`hdiutil udifrez` exists.

sign.bash
@@ -267,6 +267,10 @@ log "Convert to intermediate format needed for rez tool."
 hdiutil convert "${pkg_base}.rw.dmg" -format UDRO -o "${pkg_base}.ro"
 rm -f "${pkg_base}.rw.dmg"
 
+log "Convert back to read-only, compressed image."
+hdiutil convert "${pkg_base}.ro.dmg" -format UDZO -imagekey zlib-level=9 -ov -o "${pkg}"
+rm -f "${pkg_base}.ro.dmg"
+
 if $has_hdiutil_udifderez && $has_hdiutil_udifrez; then
   log "Re-insert SLA."
   hdiutil udifrez -xml "${sla_xml}" '' "$pkg"
@@ -274,10 +278,6 @@ if $has_hdiutil_udifderez && $has_hdiutil_udifrez; then
 else
   log "Re-insert SLA (skipping: \"hdiutil udifrez\" is not available)"
 fi
-
-log "Convert back to read-only, compressed image."
-hdiutil convert "${pkg_base}.ro.dmg" -format UDZO -imagekey zlib-level=9 -ov -o "${pkg}"
-rm -f "${pkg_base}.ro.dmg"
 
 log "Signing DMG"
 codesign --verify --verbose --display --deep -i "${id}" -s "${cert_name_app}" "${pkg}"